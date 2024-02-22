fb3b8b7d259370441199527cfa949c54a32d93c6
Pance <pan4e_stojanov@hotmail.com>
2024-02-22 14:47:17 +0100

    Weather logic

--- lib/controller/global_controller.dart
+++ lib/controller/global_controller.dart
@@ -1,51 +1,77 @@
 import 'package:geolocator/geolocator.dart';
 import 'package:get/get.dart';
+import 'package:mis_weatherapp/api/fetch_weather.dart';
+import 'package:mis_weatherapp/model/weather_data.dart';
 
 class GlobalController extends GetxController {
+  // create various variables
   final RxBool _isLoading = true.obs;
-  final RxDouble _latitude = 0.0.obs;
+  final RxDouble _lattitude = 0.0.obs;
   final RxDouble _longitude = 0.0.obs;
+  final RxInt _currentIndex = 0.obs;
 
+  // instance for them to be called
   RxBool checkLoading() => _isLoading;
-
-  RxDouble getLatitude() => _latitude;
-
+  RxDouble getLatitude() => _lattitude;
   RxDouble getLongitude() => _longitude;
+
+  final weatherData = WeatherData().obs;
+
+  WeatherData getData() {
+    return weatherData.value;
+  }
 
   @override
   void onInit() {
     if (_isLoading.isTrue) {
       getLocation();
+    } else {
+      getIndex();
     }
     super.onInit();
   }
 
-  void getLocation() async {
-    bool isServiceEnabled = await Geolocator.isLocationServiceEnabled();
+  getLocation() async {
+    bool isServiceEnabled;
     LocationPermission locationPermission;
 
+    isServiceEnabled = await Geolocator.isLocationServiceEnabled();
+    // return if service is not enabled
     if (!isServiceEnabled) {
-      return Future.error("Location services not enabled.");
+      return Future.error("Location not enabled");
     }
 
+    // status of permission
     locationPermission = await Geolocator.checkPermission();
 
     if (locationPermission == LocationPermission.deniedForever) {
-      return Future.error("Location permissions are denied forever.");
+      return Future.error("Location permission are denied forever");
     } else if (locationPermission == LocationPermission.denied) {
+      // request permission
       locationPermission = await Geolocator.requestPermission();
-
       if (locationPermission == LocationPermission.denied) {
-        return Future.error("Location permission is denied.");
+        return Future.error("Location permission is denied");
       }
     }
 
+    // getting the currentposition
     return await Geolocator.getCurrentPosition(
-            desiredAccuracy: LocationAccuracy.high)
+        desiredAccuracy: LocationAccuracy.high)
         .then((value) {
-      _latitude.value = value.latitude;
+      // update our lattitude and longitude
+      _lattitude.value = value.latitude;
       _longitude.value = value.longitude;
-      _isLoading.value = false;
+      // calling our weather api
+      return FetchWeatherAPI()
+          .processData(value.latitude, value.longitude)
+          .then((value) {
+        weatherData.value = value;
+        _isLoading.value = false;
+      });
     });
+  }
+
+  RxInt getIndex() {
+    return _currentIndex;
   }
 }
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,36 +1,77 @@
 import 'package:flutter/material.dart';
-import 'package:mis_weatherapp/controller/global_controller.dart';
 import 'package:get/get.dart';
+import 'package:mis_weatherapp/controller/global_controller.dart';
+import 'package:mis_weatherapp/utils/custom_colors.dart';
+import 'package:mis_weatherapp/widgets/comfort_level.dart';
+import 'package:mis_weatherapp/widgets/current_weather_widget.dart';
+import 'package:mis_weatherapp/widgets/daily_data_forecast.dart';
 import 'package:mis_weatherapp/widgets/header_widget.dart';
+import 'package:mis_weatherapp/widgets/hourly_data_widget.dart';
 
-class HomePage extends StatefulWidget {
-  const HomePage({super.key});
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
 
   @override
-  State<HomePage> createState() => _HomePageState();
+  State<HomeScreen> createState() => _HomeScreenState();
 }
 
-class _HomePageState extends State<HomePage> {
+class _HomeScreenState extends State<HomeScreen> {
+  // call
   final GlobalController globalController =
-      Get.put(GlobalController(), permanent: true);
-
+  Get.put(GlobalController(), permanent: true);
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
         child: Obx(() => globalController.checkLoading().isTrue
-            ? const Center(
-                child: CircularProgressIndicator(),
-              )
-            : ListView(
-                scrollDirection: Axis.vertical,
-                children: const [
-                  SizedBox(
-                    height: 20,
-                  ),
-                  HeaderWidget(),
-                ],
-              )),
+            ? Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Image.asset(
+                  "assets/icons/clouds.png",
+                  height: 200,
+                  width: 200,
+                ),
+                const CircularProgressIndicator()
+              ],
+            ))
+            : Center(
+          child: ListView(
+            scrollDirection: Axis.vertical,
+            children: [
+              const SizedBox(
+                height: 20,
+              ),
+              const HeaderWidget(),
+              // for our current temp ('current')
+              CurrentWeatherWidget(
+                weatherDataCurrent:
+                globalController.getData().getCurrentWeather(),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              HourlyDataWidget(
+                  weatherDataHourly:
+                  globalController.getData().getHourlyWeather()),
+              DailyDataForecast(
+                weatherDataDaily:
+                globalController.getData().getDailyWeather(),
+              ),
+              Container(
+                height: 1,
+                color: CustomColors.dividerLine,
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              ComfortLevel(
+                  weatherDataCurrent:
+                  globalController.getData().getCurrentWeather())
+            ],
+          ),
+        )),
       ),
     );
   }
